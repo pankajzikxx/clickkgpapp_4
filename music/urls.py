d6894756433fe6285d36e7380f59cc2211f2283e
pankaj kumar <pankajmeena.pm50@gmail.com>
2021-09-04 23:53:49 +0530

    Initial commit

---
+++ music/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import url
+from . import views
+
+app_name = 'music'
+
+urlpatterns = [
+    #/music/
+    url(r'^$', views.index, name='index'),
+
+    # /music/<album_id>/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail' ),
+    # /music/<album_id>/favourite/
+    url(r'^(?P<album_id>[0-9]+)/favorite/$', views.favorite, name='favorite' ),
+]
